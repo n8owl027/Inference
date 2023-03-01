6f4a00a339462a6a880595124aa42fdf62e85f16
nightowl <111882967+n8owl027@users.noreply.github.com>
2023-03-01 15:07:35 +0600

    Create Example 1.r

---
+++ Chapter 01/Example 1.r
@@ -0,0 +1,9 @@
+#Example: On a typical day a certain manager wants to know the average number of
+#sales calls of his sales representatives make per day. He took a random sample of 8
+#from a total of 20 representatives and collects the data from them. The obtained data
+#are in the following table.
+x<-c(4,3,2,5,3,3,1,5)
+fre<-sum(x)
+n<-8
+PI<-round(fre/n,2)
+PI
